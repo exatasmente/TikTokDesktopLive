70cec4da786bc25339560e34c0319d918bd7f207
luizneto <luiznetodev@gmail.com>
2023-10-06 14:10:03 -0300

    pages

---
+++ docs/sw.js
@@ -0,0 +1,9 @@
+// dasadasadasdsadas
+
+self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+  });
+  
+  self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+  });
