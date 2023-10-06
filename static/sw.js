4a176b25fa2b1086e3b51584cd64a62ff40950f6
luizneto <luiznetodev@gmail.com>
2023-10-06 14:15:29 -0300

    Updates

--- static/sw.js
+++
@@ -1,9 +0,0 @@
-// dasadasadasdsadas
-
-self.addEventListener('install', event => {
-    console.log('Service Worker installing.');
-  });
-  
-  self.addEventListener('activate', event => {
-    console.log('Service Worker activating.');
-  });
